4f7b761e50cc68cbab558da2cca20d593b24e7c8
VoidBlanc <angelic.noted_0g@icloud.com>
2022-03-21 10:41:57 +0800

    added for q1 and q2

---
+++ Main.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    print("Hello Glasgow!")
+    x = float(input("Enter the value for X: "))
+    y = float(input("Enter the value for Y: "))
+    average = (x + y)/2.0
+    print(average)
